25d963e03a8bcff3f13423b7b31b140eeaec9139
unknown <reemhassan160299@gmail.com>
2022-04-20 23:16:03 +0200

    add config.ru file

--- Utils/messages.js
+++ Utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text){
     return {
         username,
         text,
-        time:moment().parseZone().format('hh:mm a') 
+        time:moment(LT)
         
     }
 }
