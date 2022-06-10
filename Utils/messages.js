88a314cf2a28e89dbc29fd61cc9757bcfe62a437
reemhassan21 <reemhassan160299@gmail.com>
2022-06-10 22:26:55 +0200

    changes made to the project

--- Utils/messages.js
+++ Utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text){
     return {
         username,
         text,
-        time:moment().format('hh:mm a') 
+        time:" " + moment().hours() +":" + moment().minutes()
         
     }
 }
