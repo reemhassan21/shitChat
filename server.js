a489c5f9ef1bf1fc48221c3588fc0c22d64c93e3
reemhassan21 <reemhassan160299@gmail.com>
2024-04-22 00:44:57 +0200

    Add all my files

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
 const express = require("express");
-const path = require("path")
-const http = require("http")
-var moment = require('moment'); 
-const socketio = require("socket.io")
-const formatMessage = require('./Utils/messages')
-const {userJoin, getCurrentUser, getRoomUsers} = require('./Utils/users')
+const path = require("path");
+const http = require("http");
+var moment = require("moment");
+const socketio = require("socket.io");
+const formatMessage = require("./Utils/messages");
+const { userJoin, getCurrentUser, getRoomUsers } = require("./Utils/users");
 
 const app = express();
-const PORT  = process.env.PORT|| '3000'  ;
+const PORT = process.env.PORT || "3000";
 const server = http.createServer(app);
-const io =socketio(server);  
-app.use(express.static(path.join(__dirname,"public"))); 
-io.on("connection", socket => { 
-    socket.on('join room', ({username, room}) => { 
-        socket.broadcast.to(room).emit('message', formatMessage("ChatBot", `${username} has joined the chat`))
-      
-        socket.join(room)
+const io = socketio(server);
+app.use(express.static(path.join(__dirname, "public")));
+io.on("connection", (socket) => {
+  socket.on("join room", ({ username, room }) => {
+    socket.broadcast
+      .to(room)
+      .emit(
+        "message",
+        formatMessage("ChatBot", `${username} has joined the chat`)
+      );
 
-        socket.on('chatMessage', msg =>{
-            io.to(room).emit('message',formatMessage(username, msg))
-        })
+    socket.join(room);
 
-        socket.on('disconnect', () =>{
-            io.to(room).emit('message', formatMessage("ChatBot",`${username} has left the chat`))
-            
-        })
+    socket.on("chatMessage", (msg) => {
+      io.to(room).emit("message", formatMessage(username, msg));
+    });
 
-       
-    })
-})
+    socket.on("disconnect", () => {
+      io.to(room).emit(
+        "message",
+        formatMessage("ChatBot", `${username} has left the chat`)
+      );
+    });
+  });
+});
 
-server.listen(PORT , ()=>{console.log("localhost:3000")});
+server.listen(PORT, () => {
+  console.log("localhost:3000");
+});
